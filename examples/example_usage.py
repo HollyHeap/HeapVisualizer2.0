19054a64a0bf96bd20d2492c512c0b738756a7cf
Holly Heap <hollyheap@proton.me>
2024-08-27 09:04:16 +0200

    Add example usage script demonstrating heap operations

---
+++ examples/example_usage.py
@@ -0,0 +1,13 @@
+# examples/example_usage.py
+from heap_visualizer import visualize_heap
+from utilities.heap_utils import merge_heaps, find_min
+
+data1 = [5, 7, 9, 1, 3]
+data2 = [2, 4, 6, 8, 10]
+
+heap1 = visualize_heap(data1)
+heap2 = visualize_heap(data2)
+
+merged_heap = merge_heaps(heap1, heap2)
+print("Merged Heap:", merged_heap)
+print("Minimum:", find_min(merged_heap))
